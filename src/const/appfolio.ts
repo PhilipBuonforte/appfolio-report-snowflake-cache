bf4c497ef0d828955fa93ca9cc264e2a0e28d59e
Angryera <olegpeschinsky@gmail.com>
2025-01-24 12:43:42 +0800

    [UPDATE] Update bulk inserting'

--- src/const/appfolio.ts
+++ src/const/appfolio.ts
@@ -2,68 +2,76 @@ import { SnowFlakeInsertingMethod } from "./enum";
 
 const AppFolioReports = {
   // Correct params
-  GeneralLedger: {
-    name: "general_ledger",
-    insertMethod: SnowFlakeInsertingMethod.BulkInsertV2,
-    params: {
-      property_visibility: "all",
-      project_visibility: "all",
-      accounting_basis: "accrual",
-      posted_on_from: "01/01/2024",
-      // posted_on_to: "03/01/2024",
-      posted_on_to: new Date().toLocaleDateString("en-US"), // Dynamically sets today's date in MM/DD/YYYY format
-    },
-  },
+  // GeneralLedger: {
+  //   name: "general_ledger",
+  //   insertMethod: SnowFlakeInsertingMethod.BulkInsertV2,
+  //   params: {
+  //     property_visibility: "all",
+  //     project_visibility: "all",
+  //     accounting_basis: "accrual",
+  //     posted_on_from: "01/01/2024",
+  //     // posted_on_to: "03/01/2024",
+  //     posted_on_to: new Date().toLocaleDateString("en-US"), // Dynamically sets today's date in MM/DD/YYYY format
+  //   },
+  // },
   // Correct params
   RentRoll: {
     name: "rent_roll",
     insertMethod: SnowFlakeInsertingMethod.BulkInsert,
-    params: {
+    params: [{
       unit_visibility: "all",
       property_visibility: "all",
       as_of_to: new Date().toLocaleDateString("en-US"),
       non_revenue_units: "1",
-    },
+    }],
   },
   BudgetDetail: {
     name: "property_budget",
     insertMethod: SnowFlakeInsertingMethod.BulkInsert,
-    params: {
+    params: [{
+      period_from: "Jan 2023",
+      period_to: "Dec 2023",
+      property_visibility: "all",
+    }, {
       period_from: "Jan 2024",
+      period_to: "Dec 2024",
+      property_visibility: "all",
+    }, {
+      period_from: "Jan 2025",
       period_to: "Dec 2025",
       property_visibility: "all",
-    },
+    }],
   },
   // Correct params
   IncomeStatement: {
     name: "income_statement",
     insertMethod: SnowFlakeInsertingMethod.BulkInsert,
-    params: {
+    params: [{
       property_visibility: "all",
       accounting_basis: "Accrual",
       level_of_detail: "detail_view",
       include_zero_balance_gl_accounts: "1",
       posted_on_to: `${new Date().getFullYear()}-12`,
-    },
+    }],
   },
   // Correct params
   TenantTickler: {
     name: "tenant_tickler",
     insertMethod: SnowFlakeInsertingMethod.BulkInsert,
-    params: {
+    params: [{
       property_visibility: "all",
-      occurred_on_from: "01/01/2024",
+      occurred_on_from: "01/01/2023",
       occurred_on_to: new Date().toLocaleDateString("en-US"),
-    },
+    }],
   },
   // Correct params
   UnitVacancy: {
     name: "unit_vacancy",
     insertMethod: SnowFlakeInsertingMethod.BulkInsert,
-    params: {
+    params: [{
       unit_visibility: "all",
       property_visibility: "all",
-    },
+    }],
   },
 };
 
--- src/services/appfolioService.ts
+++ src/services/appfolioService.ts
@@ -49,7 +49,7 @@ export async function handleAppFolioData(
   paginated: boolean,
   insertMethod: SnowFlakeInsertingMethod,
   batchSize: number = 5000,
-  params: Record<string, string | number | boolean> = {}
+  params: Record<string, string | number | boolean>[]
 ): Promise<void> {
   const stagingTableName = `${tableName}_staging`; // Define the staging table name
   let nextPageUrl: string | null = null;
@@ -64,52 +64,56 @@ export async function handleAppFolioData(
 
       await dropTable(stagingTableName);
 
-      do {
-        logger.info(`[INFO] Fetching data from AppFolio API. Next page URL: ${nextPageUrl || "initial endpoint"}`);
+      if (Array.isArray(params)) {
+        for (const param of params) {
+          do {
+            logger.info(`[INFO] Fetching data from AppFolio API. Next page URL: ${nextPageUrl || "initial endpoint"}`);
 
-        // Fetch data from AppFolio API
-        const { results, next_page_url } = await fetchAppFolioData(
-          nextPageUrl || endpoint,
-          params,
-          !isFirstBatch
-        );
+            // Fetch data from AppFolio API
+            const { results, next_page_url } = await fetchAppFolioData(
+              nextPageUrl || endpoint,
+              param,
+              !isFirstBatch
+            );
 
-        logger.info(`[INFO] Fetched ${results.length} records from AppFolio API.`);
+            logger.info(`[INFO] Fetched ${results.length} records from AppFolio API.`);
 
-        // Transform the data
-        const transformedData = transformData(results);
-        logger.debug(
-          `[DEBUG] Transformed data: ${JSON.stringify(
-            transformedData[0],
-            null,
-            2
-          )} (showing first record)`
-        );
+            // Transform the data
+            const transformedData = transformData(results);
+            logger.debug(
+              `[DEBUG] Transformed data: ${JSON.stringify(
+                transformedData[0],
+                null,
+                2
+              )} (showing first record)`
+            );
 
-        // Ensure the staging table exists (only for the first batch)
-        if (isFirstBatch) {
-          logger.info(`[INFO] Ensuring staging table '${stagingTableName}' exists...`);
-          await ensureTableExists(
-            stagingTableName,
-            Object.keys(transformedData[0])
-          );
-          logger.info(`[INFO] Staging table '${stagingTableName}' is ready.`);
-          isFirstBatch = false;
+            // Ensure the staging table exists (only for the first batch)
+            if (isFirstBatch) {
+              logger.info(`[INFO] Ensuring staging table '${stagingTableName}' exists...`);
+              await ensureTableExists(
+                stagingTableName,
+                Object.keys(transformedData[0])
+              );
+              logger.info(`[INFO] Staging table '${stagingTableName}' is ready.`);
+              isFirstBatch = false;
+            }
+
+            // Insert data into Snowflake
+            if (insertMethod === SnowFlakeInsertingMethod.BatchInsert) {
+              logger.info(`[INFO] Inserting data into '${stagingTableName}' using Batch Insert.`);
+              await batchInsert(transformedData, stagingTableName, batchSize);
+            } else if (insertMethod === SnowFlakeInsertingMethod.BulkInsert) {
+              logger.info(`[INFO] Inserting data into '${stagingTableName}' using Bulk Insert.`);
+              await bulkInsert(transformedData, stagingTableName);
+            }
+            logger.info(`[INFO] Data inserted into staging table '${stagingTableName}' successfully.`);
+
+            // Update the nextPageUrl to continue fetching
+            nextPageUrl = next_page_url;
+          } while (nextPageUrl);
         }
-
-        // Insert data into Snowflake
-        if (insertMethod === SnowFlakeInsertingMethod.BatchInsert) {
-          logger.info(`[INFO] Inserting data into '${stagingTableName}' using Batch Insert.`);
-          await batchInsert(transformedData, stagingTableName, batchSize);
-        } else if (insertMethod === SnowFlakeInsertingMethod.BulkInsert) {
-          logger.info(`[INFO] Inserting data into '${stagingTableName}' using Bulk Insert.`);
-          await bulkInsert(transformedData, stagingTableName);
-        }
-        logger.info(`[INFO] Data inserted into staging table '${stagingTableName}' successfully.`);
-
-        // Update the nextPageUrl to continue fetching
-        nextPageUrl = next_page_url;
-      } while (nextPageUrl);
+      }
 
       // Drop the previous table
       logger.info(`[INFO] Dropping the old table '${tableName}'...`);
@@ -126,100 +130,101 @@ export async function handleAppFolioData(
       logger.error(`[ERROR] Error occurred while handling data for table '${tableName}': ${error.message}`);
       throw error; // Re-throw the error for further handling
     }
-  } else if (insertMethod === SnowFlakeInsertingMethod.BulkInsertV2) {
-    try {
-      await dropTable(stagingTableName);
-
-      logger.info(`[INFO] Fetching data from AppFolio API. Next page URL: ${nextPageUrl || "initial endpoint"}`);
-
-      // Fetch data from AppFolio API
-      const { results, next_page_url } = await fetchAppFolioData(
-        nextPageUrl || endpoint,
-        params,
-        false
-      );
-
-      logger.info(`[INFO] Fetched ${results.length} records from AppFolio API.`);
-
-      // Transform the data
-      const transformedData = transformData(results);
-      logger.debug(
-        `[DEBUG] Transformed data: ${JSON.stringify(
-          transformedData[0],
-          null,
-          2
-        )} (showing first record)`
-      );
-
-      logger.info(`[INFO] Ensuring staging table '${stagingTableName}' exists...`);
-      await ensureTableExists(
-        stagingTableName,
-        Object.keys(transformedData[0])
-      );
-      logger.info(`[INFO] Staging table '${stagingTableName}' is ready.`);
-
-      const uniqueFileName = generateUniqueFileName(tableName);
-      saveToCsv(transformedData, stagingTableName, uniqueFileName);
-      let csvFileNames = [];
-      csvFileNames.push(uniqueFileName);
-
-      if (next_page_url) {
-        // Parse the URL
-        const urlObj = new URL(`https://appfolio.com${next_page_url}`);
-
-        // Get the metadata_id value
-        const metadataId = urlObj.searchParams.get("metadata_id");
-
-        if (!metadataId) {
-          throw new Error("metadata_id is missing from the URL.");
-        }
-
-        let pageNumber = 1; // Start from page 1
-        let stopLoop = false; // Flag to stop the loop
-        const chunkCount = 15;
-
-        // Check if metadata_id is present
-        do {
-          // Create an array of promises for 10 iterations
-          const promises = Array.from({ length: chunkCount }, (_, index) => {
-            const currentPageNumber = pageNumber + index; // Increment page number for each iteration
-            return tryFetchData(endpoint, stagingTableName, Number(metadataId), currentPageNumber);
-          });
-
-          // Wait for all promises to resolve
-          const results = (await Promise.all(promises)).filter((result) => result.uniqueFileName !== null);
-
-          // Check if any of the results has next_page_url as null
-          stopLoop = results.some((result) => result.next_page_url === null);
-
-          csvFileNames.push(...results
-            .filter((result) => result.next_page_url !== null)
-            .map((result) => result.uniqueFileName)
-          );
-
-          await uploadFilesToSnowFlake(csvFileNames, stagingTableName);
-
-          csvFileNames = []
-
-          // Increment the page number for the next batch
-          pageNumber += chunkCount;
-        } while (!stopLoop);
-
-        // Drop the previous table
-        logger.info(`[INFO] Dropping the old table '${tableName}'...`);
-        await dropTable(tableName);
-        logger.info(`[INFO] Old table '${tableName}' dropped successfully.`);
-
-        // Rename the staging table to the original table name
-        logger.info(`[INFO] Renaming staging table '${stagingTableName}' to '${tableName}'...`);
-        await renameTable(stagingTableName, tableName);
-        logger.info(`[INFO] Staging table '${stagingTableName}' renamed to '${tableName}' successfully.`);
-
-        logger.info(`[INFO] Data handling for table '${tableName}' completed successfully.`);
-      }
-    } catch (error: any) {
-      logger.error(`[ERROR] Error occurred while handling data for table '${tableName}': ${error.message}`);
-      throw error; // Re-throw the error for further handling
-    }
   }
+  // else if (insertMethod === SnowFlakeInsertingMethod.BulkInsertV2) {
+  //   try {
+  //     await dropTable(stagingTableName);
+
+  //     logger.info(`[INFO] Fetching data from AppFolio API. Next page URL: ${nextPageUrl || "initial endpoint"}`);
+
+  //     // Fetch data from AppFolio API
+  //     const { results, next_page_url } = await fetchAppFolioData(
+  //       nextPageUrl || endpoint,
+  //       params,
+  //       false
+  //     );
+
+  //     logger.info(`[INFO] Fetched ${results.length} records from AppFolio API.`);
+
+  //     // Transform the data
+  //     const transformedData = transformData(results);
+  //     logger.debug(
+  //       `[DEBUG] Transformed data: ${JSON.stringify(
+  //         transformedData[0],
+  //         null,
+  //         2
+  //       )} (showing first record)`
+  //     );
+
+  //     logger.info(`[INFO] Ensuring staging table '${stagingTableName}' exists...`);
+  //     await ensureTableExists(
+  //       stagingTableName,
+  //       Object.keys(transformedData[0])
+  //     );
+  //     logger.info(`[INFO] Staging table '${stagingTableName}' is ready.`);
+
+  //     const uniqueFileName = generateUniqueFileName(tableName);
+  //     saveToCsv(transformedData, stagingTableName, uniqueFileName);
+  //     let csvFileNames = [];
+  //     csvFileNames.push(uniqueFileName);
+
+  //     if (next_page_url) {
+  //       // Parse the URL
+  //       const urlObj = new URL(`https://appfolio.com${next_page_url}`);
+
+  //       // Get the metadata_id value
+  //       const metadataId = urlObj.searchParams.get("metadata_id");
+
+  //       if (!metadataId) {
+  //         throw new Error("metadata_id is missing from the URL.");
+  //       }
+
+  //       let pageNumber = 1; // Start from page 1
+  //       let stopLoop = false; // Flag to stop the loop
+  //       const chunkCount = 15;
+
+  //       // Check if metadata_id is present
+  //       do {
+  //         // Create an array of promises for 10 iterations
+  //         const promises = Array.from({ length: chunkCount }, (_, index) => {
+  //           const currentPageNumber = pageNumber + index; // Increment page number for each iteration
+  //           return tryFetchData(endpoint, stagingTableName, Number(metadataId), currentPageNumber);
+  //         });
+
+  //         // Wait for all promises to resolve
+  //         const results = (await Promise.all(promises)).filter((result) => result.uniqueFileName !== null);
+
+  //         // Check if any of the results has next_page_url as null
+  //         stopLoop = results.some((result) => result.next_page_url === null);
+
+  //         csvFileNames.push(...results
+  //           .filter((result) => result.next_page_url !== null)
+  //           .map((result) => result.uniqueFileName)
+  //         );
+
+  //         await uploadFilesToSnowFlake(csvFileNames, stagingTableName);
+
+  //         csvFileNames = []
+
+  //         // Increment the page number for the next batch
+  //         pageNumber += chunkCount;
+  //       } while (!stopLoop);
+
+  //       // Drop the previous table
+  //       logger.info(`[INFO] Dropping the old table '${tableName}'...`);
+  //       await dropTable(tableName);
+  //       logger.info(`[INFO] Old table '${tableName}' dropped successfully.`);
+
+  //       // Rename the staging table to the original table name
+  //       logger.info(`[INFO] Renaming staging table '${stagingTableName}' to '${tableName}'...`);
+  //       await renameTable(stagingTableName, tableName);
+  //       logger.info(`[INFO] Staging table '${stagingTableName}' renamed to '${tableName}' successfully.`);
+
+  //       logger.info(`[INFO] Data handling for table '${tableName}' completed successfully.`);
+  //     }
+  //   } catch (error: any) {
+  //     logger.error(`[ERROR] Error occurred while handling data for table '${tableName}': ${error.message}`);
+  //     throw error; // Re-throw the error for further handling
+  //   }
+  // }
 }
