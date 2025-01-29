2b727f11690129a15ad17fb582f945612a92ce7a
Angryera <olegpeschinsky@gmail.com>
2025-01-29 11:42:16 +0800

    [UPDATE] Activate other reports including General Ledger, BudgetDetail

--- src/const/appfolio.ts
+++ src/const/appfolio.ts
@@ -9,83 +9,81 @@ const GenerateAppFolioReports = () => {
   const agedReceivablesParams = generateAgedReceivablesParams();
   const rentRollParams = generateRentRollParams();
 
-  console.log(rentRollParams.isFirstRun);
-  console.log(agedReceivablesParams.isFirstRun);
   return {
-    // // Correct params
-    // GeneralLedger: {
-    //   name: "general_ledger",
-    //   insertMethod: SnowFlakeInsertingMethod.BulkInsert,
-    //   params: generateGeneralLedgerParams(
-    //     "01/01/2023",
-    //     new Date().toLocaleDateString("en-US"),
-    //     2
-    //   ),
-    // },
+    // Correct params
+    GeneralLedger: {
+      name: "general_ledger",
+      insertMethod: SnowFlakeInsertingMethod.BulkInsert,
+      params: generateGeneralLedgerParams(
+        "01/01/2023",
+        new Date().toLocaleDateString("en-US"),
+        2
+      ),
+    },
 
-    // // Correct params
-    // BudgetDetail: {
-    //   name: "property_budget",
-    //   insertMethod: SnowFlakeInsertingMethod.BulkInsert,
-    //   params: [
-    //     {
-    //       year_to: "2023-12",
-    //       property_visibility: "all",
-    //     },
-    //     {
-    //       year_to: "2024-12",
-    //       property_visibility: "all",
-    //     },
-    //     {
-    //       year_to: "2025-12",
-    //       property_visibility: "all",
-    //     },
-    //   ],
-    // },
-    // // Correct params
-    // IncomeStatement: {
-    //   name: "income_statement",
-    //   insertMethod: SnowFlakeInsertingMethod.BulkInsert,
-    //   params: [
-    //     {
-    //       property_visibility: "all",
-    //       accounting_basis: "Accrual",
-    //       level_of_detail: "detail_view",
-    //       include_zero_balance_gl_accounts: "1",
-    //       posted_on_to: `${new Date().getFullYear()}-12`,
-    //     },
-    //   ],
-    // },
-    // // Correct params
-    // TenantTickler: {
-    //   name: "tenant_tickler",
-    //   insertMethod: SnowFlakeInsertingMethod.BulkInsert,
-    //   params: [
-    //     {
-    //       property_visibility: "all",
-    //       occurred_on_from: "01/01/2023",
-    //       occurred_on_to: new Date().toLocaleDateString("en-US"),
-    //     },
-    //   ],
-    // },
-    // // Correct params
-    // UnitVacancy: {
-    //   name: "unit_vacancy",
-    //   insertMethod: SnowFlakeInsertingMethod.BulkInsert,
-    //   params: [
-    //     {
-    //       unit_visibility: "all",
-    //       property_visibility: "all",
-    //     },
-    //   ],
-    // },
+    // Correct params
+    BudgetDetail: {
+      name: "property_budget",
+      insertMethod: SnowFlakeInsertingMethod.BulkInsert,
+      params: [
+        {
+          year_to: "2023-12",
+          property_visibility: "all",
+        },
+        {
+          year_to: "2024-12",
+          property_visibility: "all",
+        },
+        {
+          year_to: "2025-12",
+          property_visibility: "all",
+        },
+      ],
+    },
+    // Correct params
+    IncomeStatement: {
+      name: "income_statement",
+      insertMethod: SnowFlakeInsertingMethod.BulkInsert,
+      params: [
+        {
+          property_visibility: "all",
+          accounting_basis: "Accrual",
+          level_of_detail: "detail_view",
+          include_zero_balance_gl_accounts: "1",
+          posted_on_to: `${new Date().getFullYear()}-12`,
+        },
+      ],
+    },
+    // Correct params
+    TenantTickler: {
+      name: "tenant_tickler",
+      insertMethod: SnowFlakeInsertingMethod.BulkInsert,
+      params: [
+        {
+          property_visibility: "all",
+          occurred_on_from: "01/01/2023",
+          occurred_on_to: new Date().toLocaleDateString("en-US"),
+        },
+      ],
+    },
+    // Correct params
+    UnitVacancy: {
+      name: "unit_vacancy",
+      insertMethod: SnowFlakeInsertingMethod.BulkInsert,
+      params: [
+        {
+          unit_visibility: "all",
+          property_visibility: "all",
+        },
+      ],
+    },
 
-    // // Correct params
-    // ChartOfAccounts: {
-    //   name: "chart_of_accounts",
-    //   insertMethod: SnowFlakeInsertingMethod.BulkInsert,
-    //   params: [{}],
-    // },
+    // Correct params
+    ChartOfAccounts: {
+      name: "chart_of_accounts",
+      insertMethod: SnowFlakeInsertingMethod.BulkInsert,
+      params: [{}],
+    },
 
     AgedReceivableDetail: {
       name: "aged_receivables_detail",
